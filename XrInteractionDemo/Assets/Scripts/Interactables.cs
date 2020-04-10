a42403d444dc9fbff75330e1e5a460c6a3f775ee
Danish Ali <syeddanishdev@gmail.com>
2020-04-10 19:40:21 +0200

    Controller Interactions Added
    Intercation with laser pointer has been added.

---
+++ XrInteractionDemo/Assets/Scripts/Interactables.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Interactables : MonoBehaviour
+{
+    private XRGrabInteractable grabInteractable = null;
+
+    [SerializeField] Material selected = null;
+    [SerializeField] Material unSelected = null;
+
+    private MeshRenderer meshRenderer = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        grabInteractable.onActivate.AddListener(grabObject);
+        grabInteractable.onDeactivate.AddListener(releaseObject);
+    }
+    private void OnDestroy()
+    {
+        grabInteractable.onActivate.RemoveListener(grabObject);
+        grabInteractable.onDeactivate.RemoveListener(releaseObject);
+    }
+
+    // Update is called once per frame
+    void grabObject(XRBaseInteractor interactor)
+    { 
+    
+    }
+    void releaseObject(XRBaseInteractor interactor)
+    {
+
+    }
+}
